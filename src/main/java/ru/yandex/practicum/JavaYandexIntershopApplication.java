e86dabd78027b83822558871e8608001d01f7873
Alex <badirbis@yandex.ru>
2025-06-15 14:18:13 +0300

    Init

---
+++ src/main/java/ru/yandex/practicum/JavaYandexIntershopApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaYandexIntershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaYandexIntershopApplication.class, args);
+	}
+
+}
